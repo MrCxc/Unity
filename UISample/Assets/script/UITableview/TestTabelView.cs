922b383370f019c3ad73b63ccd84e24e12f38bae
CaoXingCai <caoxingcai@gmail.com>
2017-04-13 09:53:34 +0800

    create UISample project

---
+++ UISample/Assets/script/UITableview/TestTabelView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTabelView : MonoBehaviour {
+
+	void Start () {
+        TableView tb = TableView.Create(gameObject, "Item");
+
+        Hashtable data = new Hashtable();
+        data.Add(1, "111");
+        data.Add(2, "222");
+        data.Add(3, "333");
+        data.Add(4, "444");
+        data.Add(5, "555");
+        data.Add(6, "666");
+        data.Add(7, "666");
+        data.Add(8, "666");
+
+        tb.UpdateItems(data);
+	}
+}
---
+++ UISample/Assets/script/UITableview/UISuperTableCell.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISuperTableCell : MonoBehaviour {
+
+	//公共方法
+	public static UISuperTableCell createCell(string perfabName){
+		GameObject _cellPerfab = Resources.Load (perfabName) as GameObject;
+		GameObject _object = GameObject.Instantiate (_cellPerfab) as GameObject;
+		UISuperTableCell _cell = null;
+		if (_object != null) {
+			_cell = _object.GetComponent(typeof(UISuperTableCell)) as UISuperTableCell;
+			_cell.cellGameObject = _object;
+		} else {
+			Debug.Log ("出错了");
+		}
+		return _cell;
+	}
+
+	public GameObject cellGameObject {
+		get;
+		set;
+	}
+
+	public int cellIndex {
+		get;
+		set;
+	}
+		
+}
