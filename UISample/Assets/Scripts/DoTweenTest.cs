bdaad915daefb8df32ecc7c754888dd3566de783
CaoXingCai <caoxingcai@gmail.com>
2017-05-16 18:23:49 +0800

    code update

--- UISample/Assets/Scripts/DoTweenTest.cs
+++ UISample/Assets/Scripts/DoTweenTest.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using DG.Tweening; 
 
 public class DoTweenTest : MonoBehaviour {
+    /*
     public Vector3 vec3 = Vector3.zero;
     public float fValue = 0f;
     public int iValue = 0;
@@ -10,9 +12,14 @@ public class DoTweenTest : MonoBehaviour {
     public RectTransform rectTrans;
 
     GameObject obj;
-    bool bClick;
 
     public GameObject cubeObj;
+    */
+
+    public Text m_text;
+    public Camera m_camera;
+
+    bool bClick;
 
 	void Start () {
         // Lambda 
@@ -23,8 +30,10 @@ public class DoTweenTest : MonoBehaviour {
         DOTween.To(()=> iValue, x=> iValue = x, 10, 3f);
         */
 
+        /*
         obj = gameObject;
         bClick = false;
+        */
 
         /*
         Tween twn = rectTrans.DOLocalMove(new Vector3(1000, 1000, 0), 0.3f);
@@ -35,12 +44,38 @@ public class DoTweenTest : MonoBehaviour {
         //cubeObj.transform.DOLocalMoveX(300f, 3f).From(); // 绝对位置
         //cubeObj.transform.DOLocalMoveX(300f, 3f).From(true);// 相对位置
 
-        cubeObj.transform.DOBlendableLocalMoveBy(new Vector3(300f, 300f, 0f), 3f);
+        //cubeObj.transform.DOBlendableLocalMoveBy(new Vector3(300f, 300f, 0f), 3f);
+
+        /*
+        // 动画设置
+        Tween twn = cubeObj.transform.DOLocalMoveX(-300f, 2f).From();
+        twn.SetEase(Ease.OutBounce);
+        twn.OnComplete(AnimationComplete);
+        */
+
+        // text 
+        m_text.text = "";
+
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(m_text.DOText("The afternoon grows light because at last.", 2f));
+        mySequence.Append(m_text.DOColor(Color.red, 2f));
+        mySequence.Append(m_text.DOFade(100f, 0.3f));
+        mySequence.Insert(4.0f, m_text.DOBlendableColor(new Color(1.0f, 1.0f, 1.0f), 3.0f));
+        mySequence.Join(m_camera.DOShakePosition(1.0f, 10.0f));
+
+        //StartCoroutine(SomeCoroutine());
+
+        mySequence.Play();
 	}
 	
 	void Update () {
-	
+
 	}
+
+    void AnimationComplete()
+    {
+        Debug.Log("finished.");
+    }
 
     public void OnClick()
     {
@@ -61,5 +96,22 @@ public class DoTweenTest : MonoBehaviour {
         }
 
         bClick = !bClick;
+
+        //m_camera.DOShakePosition(1.0f, 10.0f);
+        //m_camera.DOShakeRotation(1.0f);
+
+        /*
+        m_camera.clearFlags = CameraClearFlags.SolidColor;
+        m_camera.DOColor(new Color(1.0f, 0.0f, 0.0f), 2f);
+        */
+    }
+
+    IEnumerator SomeCoroutine()
+    {
+        Debug.Log("-------------");
+        Tween myTween = transform.DOMoveX(45, 1);
+        yield return myTween.WaitForCompletion();
+        // This log will happen after the tween has completed
+        Debug.Log("Tween completed!");
     }
 }
